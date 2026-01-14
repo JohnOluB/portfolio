f49c97509a27b809ce9d4ecd58279535c1f148d7
JohnOluB <adebayodeolu90@gmail.com>
2026-01-14 16:25:34 +0100

    feat: add react router packages

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Hero from './Components/Hero/hero'
 import NavBar from './Components/NavBar/NavBar'
+import {Route} from 'react-router'
 
 const App = () => {
   return (
     <>
+    <Route path="">
       <NavBar />
+    </Route>
+    <Route path="./Hero"> 
       <Hero />
+    </Route> 
     </>
   )
 }
