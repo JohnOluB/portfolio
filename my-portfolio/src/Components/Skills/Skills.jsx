2a980b891ec5abc280415b1c53a77d5360a2c9fd
JohnOluB <adebayodeolu90@gmail.com>
2026-01-23 21:12:22 +0100

    add skills component styles

--- my-portfolio/src/Components/Skills/Skills.jsx
+++ my-portfolio/src/Components/Skills/Skills.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='flex justify-center items-center gap-12'>
+        <div className='h-30 w-30 border flex justify-center items-center'>React</div>
+        <div className='h-30 w-30 border flex justify-center items-center'>Nextjs</div>
+        <div className='h-30 w-30 border flex justify-center items-center'>Tailwind</div>
+        <div className='h-30 w-30 border flex justify-center items-center'>Javascript</div>
+        <div className='h-30 w-30 border flex justify-center items-center'>Typescript</div>
+        <div className='h-30 w-30 border flex justify-center items-center'>MongoDB</div>
+        <div className='h-30 w-30 border flex justify-center items-center'>Express</div>
+        <div className='h-30 w-30 border flex justify-center items-center'>Nodejs</div>
+    </div>
   )
 }
 
