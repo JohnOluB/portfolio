2b382aa4fe36bb8dc4cf02133f3e7ae4c7d9ff88
JohnOluB <adebayodeolu90@gmail.com>
2026-01-30 22:35:41 +0100

    feat: Add NavBar components for the portfolio layout.

--- my-portfolio/src/Components/NavBar/NavBar.jsx
+++ my-portfolio/src/Components/NavBar/NavBar.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-      <div className="flex justify-center items-center gap-8 mt-5 text-2xl">
+      <div className="flex mt-4 text-2xl">
+         <span className='flex-1 ml-3'>JOHN ADEBAYO</span>
+        <div className='mr-20'>
           <span>Home</span>
+          <span>About</span>
           <span>Projects</span>
-          <span>Skills</span>
+          <span>Articles</span>
           <span>Contact</span>
+          <img src="" alt="oi" />
+        </div>
       </div>
   )
 }
