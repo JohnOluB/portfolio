98bd2f0adc17d809d058f98d5c5f72f2bf47a39c
JohnOluB <adebayodeolu90@gmail.com>
2026-01-31 22:47:18 +0100

    feat: add Projects component with placeholder images.

--- my-portfolio/src/Components/Projects/Projects.jsx
+++ my-portfolio/src/Components/Projects/Projects.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Baselance from '../../assets/baselancejpg.png'
 
 const Projects = () => {
   return (
-    <div>
-    <div>project1</div>
-    <div>P2</div>
-    <div>p3</div>
-    <div>p4</div>
+    <div className='flex gap-12 mt-40 ml-10'>
+    <div className='h-120 w-100 border-grey-400 border rounded-2xl'><img src={Baselance} alt="baselance image" className='w-120 h-60 rounded-2xl'/></div>
+     <div className='h-120 w-100 border-grey-400 border rounded-2xl'><img src={Baselance} alt="baselance image" className='w-120 h-60 rounded-2xl'/></div>
+      <div className='h-120 w-100 border-grey-400 border rounded-2xl'><img src={Baselance} alt="baselance image" className='w-120 h-60 rounded-2xl'/></div>
+
     </div>
   )
 }
