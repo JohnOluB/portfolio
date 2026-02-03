1aa58cb3ed7da761e086e3cef176efaed42e2f96
JohnOluB <adebayodeolu90@gmail.com>
2026-02-03 19:57:49 +0100

    feat: add routing to navbar components

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router'
+import Layout from './Layout.jsx'
+import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
+import Skills from './Components/Skills/Skills'
+import Projects from './Components/Projects/Projects'
+import Articles from './Components/Articles/Article'
+import Contact from './Components/COntact/Contact'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Hero/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='skills' element={<Skills/>}/>
+      <Route path='projects' element={<Projects/>}/>
+      <Route path='articles' element={<Articles/>}/>
+      <Route path='contact' element={<Contact/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
