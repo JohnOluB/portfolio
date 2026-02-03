649c9c50267e861e89e0c84d1b20689f4c6889e4
JohnOluB <adebayodeolu90@gmail.com>
2026-02-03 21:01:31 +0100

    feat: add nav element to navbar components

--- my-portfolio/src/Components/NavBar/NavBar.jsx
+++ my-portfolio/src/Components/NavBar/NavBar.jsx
@@ -4,17 +4,18 @@ import {NavLink} from 'react-router'
 
 const NavBar = () => {
   return (
-      <div className="flex mt-4 text-2xl">
+      <nav className="flex mt-4 text-2xl">
          <span className='flex-1 ml-3'>JOHN ADEBAYO</span>
         <div className='mr-20'>
-          <NavLink to='/' className={({isActive})=>{ {isActive ? 'text-orange-700':'text-grey-700'}}}>Home</NavLink>
+          <NavLink to='/' className={({isActive})=>isActive ? 'text-orange-700':'text-grey-700'}>Home</NavLink>
           <NavLink to='about'>About</NavLink>
           <NavLink to='Projects'>Projects</NavLink>
           <NavLink to='Articles'>Articles</NavLink>
           <NavLink to='Contact'>Contact</NavLink>  
           <img src="" alt="oi" />
         </div>
-      </div>
+      </nav>
+    
   )
 }
 
