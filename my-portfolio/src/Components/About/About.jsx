39cc185d09f5bdc4c38b180ff74fbcfd3221ccdc
JohnOluB <adebayodeolu90@gmail.com>
2026-02-05 22:36:24 +0100

    fix: adjust punctuation in about component

--- my-portfolio/src/Components/About/About.jsx
+++ my-portfolio/src/Components/About/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className='flex'>
         <div><img src="" alt="profile" /></div>
-        <span className='text-3xl'>I specialize in crafting websites that guides your visitors through a discovery journey and converts them to paying customers. My approach focuses on creating scalable, high performing solutions tailored to both user needs and business objectives. By prioritizing performance, accessibility and responsiveness. I strive to deliver experiences that not only engage users but also drive tangible results
+        <span className='text-3xl'>I specialize in crafting websites that guides your visitors through a discovery journey and converts them to paying customers. My approach focuses on creating scalable, high performing solutions tailored to both user needs and business objectives by prioritizing performance, accessibility and responsiveness. I strive to deliver experiences that not only engage users but also drive tangible results
         </span>
     </div>
   )
