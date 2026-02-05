ebea7a5abfaef94e5e36ffa1daa7fb8d5c489a02
JohnOluB <adebayodeolu90@gmail.com>
2026-02-05 22:36:53 +0100

    feat: add contact component

--- my-portfolio/src/Components/Contact/Contact.jsx
+++ my-portfolio/src/Components/Contact/Contact.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='flex center items-center justify-center m-0 gap-30'>
+      <div>
+        <h1>Let's Create Together</h1>
+        <span>I'm always excited to work with fellow developers, potential clients, and anyone passionate about technology. Whether you have a project in mind or you just want to say hello, I'm here to collaborate</span>
+      </div>
+      <div>
+        <h1>Quick Links</h1>
+        <div>
+          <div className='flex'>
+          <li>About</li>
+          <li>Articles</li>
+          <li>Resume</li>
+          </div>
+          <div>Projects</div>
+          <li>Contact</li>
+          <li>Skills</li>
+        </div>
+      </div>
+      <div><h1>Get in Touch</h1>
+      <div className='flex-col'>
+        <span>Have a project in mind? Let's discuss how we can work together to bring your projects back to life</span>
+        </div>
+        <button className='pointer-cursor'><img></img>Send me a mail</button>
+      </div>
+      
+    </div>
   )
 }
 
