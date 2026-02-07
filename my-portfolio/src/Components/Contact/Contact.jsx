19669a0ee020f48e50cb291422b2a3ec06a52732
JohnOluB <adebayodeolu90@gmail.com>
2026-02-07 14:10:51 +0100

    feat: add styling to contact component

--- my-portfolio/src/Components/Contact/Contact.jsx
+++ my-portfolio/src/Components/Contact/Contact.jsx
@@ -2,29 +2,31 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='flex center items-center justify-center m-0 gap-30'>
+    <div className='flex justify-center items-center mt-[150px] gap-30 text-3xl'>
       <div>
         <h1>Let's Create Together</h1>
         <span>I'm always excited to work with fellow developers, potential clients, and anyone passionate about technology. Whether you have a project in mind or you just want to say hello, I'm here to collaborate</span>
       </div>
       <div>
-        <h1>Quick Links</h1>
-        <div>
-          <div className='flex'>
-          <li>About</li>
-          <li>Articles</li>
-          <li>Resume</li>
+        <h1 className='flex justify-center items-center'>Quick Links</h1>
+        <div className='flex gap-10'>
+          <div className=''>
+            <p>About</p>
+            <p>Articles</p>
+            <p>Resume</p>
           </div>
-          <div>Projects</div>
-          <li>Contact</li>
-          <li>Skills</li>
+          <div className=''>
+            <p>Projects</p>
+            <p>Contact</p>
+            <p>Skills</p>
+          </div>
         </div>
       </div>
       <div><h1>Get in Touch</h1>
       <div className='flex-col'>
         <span>Have a project in mind? Let's discuss how we can work together to bring your projects back to life</span>
         </div>
-        <button className='pointer-cursor'><img></img>Send me a mail</button>
+        <button className='cursor-pointer'><img></img>Send me a mail</button>
       </div>
       
     </div>
